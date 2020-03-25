796a3305c925ab2300e24471fa8e59879469670e
Jacob Dannhausen-Brun <jdannhausenbrun@gmail.com>
2020-03-24 19:00:43 -0500

    Finished demo UI

--- app/src/main/java/com/cs411databases/databasefinalproject/ui/insert/InsertFragment.kt
+++ app/src/main/java/com/cs411databases/databasefinalproject/ui/insert/InsertFragment.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import com.cs411databases.databasefinalproject.R
 
 class InsertFragment : Fragment() {
+    private var currentSpinnerSelection: Int = -1
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -23,6 +26,64 @@ class InsertFragment : Fragment() {
         val spinnerAdapter: ArrayAdapter<String> = ArrayAdapter(context!!, android.R.layout.simple_spinner_item, spinnerItems)
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = spinnerAdapter
+
+        val editText1: EditText = root.findViewById(R.id.editText1)
+        val editText2: EditText = root.findViewById(R.id.editText2)
+        val editText3: EditText = root.findViewById(R.id.editText3)
+        val editText4: EditText = root.findViewById(R.id.editText4)
+        val editText5: EditText = root.findViewById(R.id.editText5)
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when (position) {
+                    0 -> {
+                        editText1.hint = "BrandID"
+                        editText2.hint = "BrandName"
+                        editText3.hint = "N/A"
+                        editText4.hint = "N/A"
+                        editText5.hint = "N/A"
+                        currentSpinnerSelection = 0
+                    }
+                    1 -> {
+                        editText1.hint = "TransactionID"
+                        editText2.hint = "ProductOfferingID"
+                        editText3.hint = "TransactionDate"
+                        editText4.hint = "TransactionPrice"
+                        editText5.hint = "IsReturn"
+                        currentSpinnerSelection = 1
+                    }
+                    2 -> {
+                        editText1.hint = "RetailerID"
+                        editText2.hint = "RetailerName"
+                        editText3.hint = "RetailerCategory"
+                        editText4.hint = "N/A"
+                        editText5.hint = "N/A"
+                        currentSpinnerSelection = 2
+                    }
+                    3 -> {
+                        editText1.hint = "ProductID"
+                        editText2.hint = "BrandID"
+                        editText3.hint = "ProductName"
+                        editText4.hint = "ProductType"
+                        editText5.hint = "N/A"
+                        currentSpinnerSelection = 3
+                    }
+                    4 -> {
+                        editText1.hint = "ProductOfferingID"
+                        editText2.hint = "RetailerID"
+                        editText3.hint = "ProductID"
+                        editText4.hint = "Price"
+                        editText5.hint = "DiscountPrice"
+                        currentSpinnerSelection = 4
+                    }
+                    else -> { // Note the block
+
+                    }
+                }
+            }
+        }
 
         return root
     }
--- app/src/main/java/com/cs411databases/databasefinalproject/ui/view/ViewFragment.kt
+++ app/src/main/java/com/cs411databases/databasefinalproject/ui/view/ViewFragment.kt
@@ -1,14 +1,19 @@
 package com.cs411databases.databasefinalproject.ui.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.cs411databases.databasefinalproject.R
+import com.cs411databases.databasefinalproject.objects.Brand
 
 class ViewFragment : Fragment() {
 
@@ -26,6 +31,83 @@ class ViewFragment : Fragment() {
         spinner.adapter = spinnerAdapter
 
         val recyclerView: RecyclerView = root.findViewById(R.id.view_recyclerview)
+
+        val brand1 = Brand()
+        brand1.brandID = 1L
+        brand1.brandName = "Brand 1"
+        val brand2 = Brand()
+        brand2.brandID = 2L
+        brand2.brandName = "Brand 2"
+        val brand3 = Brand()
+        brand3.brandID = 3L
+        brand3.brandName = "Brand 3"
+        val brand4 = Brand()
+        brand4.brandID = 4L
+        brand4.brandName = "Brand 4"
+        val brand5 = Brand()
+        brand5.brandID = 5L
+        brand5.brandName = "Brand 5"
+        val brand6 = Brand()
+        brand6.brandID = 6L
+        brand6.brandName = "Brand 6"
+        val brand7 = Brand()
+        brand7.brandID = 7L
+        brand7.brandName = "Brand 7"
+        val brand8 = Brand()
+        brand8.brandID = 8L
+        brand8.brandName = "Brand 8"
+        var listOfItems: MutableList<Any> = mutableListOf(brand1, brand2, brand3, brand4, brand5, brand6, brand7, brand8)
+        val listAdapter = ListAdapter(listOfItems)
+
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = listAdapter
+        }
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when (position) {
+                    0 -> print("x == 0")
+                    1 -> print("x == 1")
+                    2 -> print("x == 2")
+                    3 -> print("x == 3")
+                    4 -> print("x == 4")
+                    else -> { // Note the block
+
+                    }
+                }
+            }
+        }
+
+        listAdapter.notifyDataSetChanged()
+
         return root
+    }
+
+    class ListAdapter(private val list: List<Any>): RecyclerView.Adapter<ListAdapter.ObjectViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ObjectViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            return ObjectViewHolder(inflater, parent)
+        }
+
+        override fun onBindViewHolder(holder: ObjectViewHolder, position: Int) {
+            val obj: Any = list[position]
+            holder.bind(obj)
+        }
+
+        override fun getItemCount(): Int = list.size
+
+        class ObjectViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+            RecyclerView.ViewHolder(inflater.inflate(R.layout.view_list_item, parent, false)) {
+
+            private var textView: TextView = itemView.findViewById(R.id.list_item_text)
+
+            fun bind(obj: Any) {
+                textView.text = obj.toString()
+            }
+        }
     }
 }
